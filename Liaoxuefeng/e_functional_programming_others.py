ac78684245b01e52287a0866423baa62fbfbef88
Daicy0213 <372454567@qq.com>
2021-02-25 16:37:45 +0800

    Python学习

--- Liaoxuefeng/e_functional_programming_others.py
+++ Liaoxuefeng/e_functional_programming_others.py
@@ -1,6 +1,7 @@
 """
 函数式编程
 """
+import time, functools
 
 """
 二.返回函数
@@ -20,6 +21,7 @@ def calc_sum(*args):
     return ax
 
 
+print('##### 闭包 START #####')
 """
 1.闭包
 如果不需要立刻求和，而是在后面的代码中，根据需要再计算，调用lazy_sum()时，返回的并不是求和结果，而是求和函数
@@ -77,11 +79,12 @@ def createCounter():
     return counter
 
 
+print('##### 闭包 END #####')
 """
 2.匿名函数
 在Python中，对匿名函数提供了有限支持。还是以map()函数为例，计算f(x)=x2时，除了定义一个f(x)的函数外，还可以直接传入匿名函数
 """
-
+print('##### 匿名函数 START #####')
 print(list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
 
 """
@@ -92,7 +95,133 @@ print(list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
 
 f = lambda x: x * x
 print(f(5))
-
+print('##### 匿名函数 END #####')
 """
 装饰器Decorator
+本质上，decorator就是一个返回函数的高阶函数。
+https://www.liaoxuefeng.com/wiki/1016959663602400/1017451662295584
 """
+print('##### 装饰器 START #####')
+
+
+# 定义一个能打印日志的decorator
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+
+    return wrapper
+
+
+# 因为它是一个decorator，所以接受一个函数作为参数，并返回一个函数。
+# 我们要借助Python的@语法，把decorator置于函数的定义处
+
+
+@log
+def now():
+    print(time.asctime(time.localtime(time.time())))
+
+
+# 相当于now = log(now)
+now()
+print('name: ', now.__name__)
+
+"""
+由于log()是一个decorator，返回一个函数，所以，原来的now()函数仍然存在，
+只是现在同名的now变量指向了新的函数，于是调用now()将执行新函数，即在log()函数中返回的wrapper()函数。
+wrapper()函数的参数定义是(*args, **kw)，因此，wrapper()函数可以接受任意参数的调用。
+在wrapper()函数内，首先打印日志，再紧接着调用原始函数。
+"""
+
+
+# 如果decorator本身需要传入参数，那就需要编写一个返回decorator的高阶函数，写出来会更复杂。
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+
+        return wrapper
+
+    return decorator
+
+
+@log('execute')
+def now():
+    print(time.asctime(time.localtime(time.time())))
+
+
+# 相当于now = log('execute')(now)
+now()
+print('name: ', now.__name__)
+
+"""
+我们来剖析上面的语句，首先执行log('execute')，返回的是decorator函数，再调用返回的函数，参数是now函数，返回值最终是wrapper函数。
+但是它们的__name__已经从原来的'now'变成了'wrapper', 这不符合装饰器的定义, 即只增强函数而不修改函数
+Python内置的@functools.wraps 提供了对应的功能, 不需要编写wrapper.__name__ = func.__name__这样的代码
+"""
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+
+    return wrapper
+
+
+@log
+def now():
+    print(time.asctime(time.localtime(time.time())))
+
+
+now()
+print('name: ', now.__name__)
+
+"""
+请设计一个decorator，它可作用于任何函数上，并打印该函数的执行时间：
+"""
+
+
+def metric(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kw):
+        start = time.time()
+        re = fn(*args, **kw)  # 执行函数
+        cost = time.time() - start
+        print('%s executed in %s ms' % (fn.__name__, cost))
+        return re
+
+    return wrapper
+
+
+@metric
+def Kobe():
+    print("MVP")
+
+
+Kobe()
+print('name: ', Kobe.__name__)
+print('##### 装饰器 END #####')
+
+"""
+偏函数
+Python的#functools模块提供了很多有用的功能，其中一个就是偏函数（Partial function）。要注意，这里的偏函数和数学意义上的偏函数不一样。
+在介绍函数参数的时候，我们讲到，通过设定参数的默认值，可以降低函数调用的难度。而偏函数也可以做到这一点。
+"""
+print('##### 偏函数 START #####')
+# int()函数可以把字符串转换为整数，当仅传入字符串时，int()函数默认按十进制转换
+# 但int()函数还提供额外的base参数，默认值为10。如果传入base参数，就可以做N进制的转换
+print(int('12345', base=8))  # 将八进制的12345转换为十进制
+print(int('F32CD', 16))  # 将十六进制的F32CD转换为十进制
+
+# functools.partial就是帮助我们创建一个偏函数, 把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单
+# 二进制转换的函数
+int2 = functools.partial(int, base=2)
+print(int2('10000'))
+
+# 最后，创建偏函数时，实际上可以接收函数对象、*args和**kw这3个参数
+# int2函数中, 相当于kw = { 'base': 2 }
+
+print('##### 偏函数 END #####')
---
+++ Liaoxuefeng/f_module.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 标准注释，第1行注释可以让这个hello.py文件直接在Unix/Linux/Mac上运行
+# 第2行注释表示.py文件本身使用标准UTF-8编码；
+
+""" a standard module """  # 表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释
+
+# 类似__xxx__这样的变量是特殊变量，可以被直接引用，但是有特殊用途
+# 比如__author__，__name__就是特殊变量，hello模块定义的文档注释也可以用特殊变量__doc__访问，我们自己的变量一般不要用这种变量名
+# 类似_xxx和__xxx这样的函数或变量就是非公开的（private），不应该被直接引用
+# Python并没有一种方法可以完全限制访问private函数或变量，但是，从编程习惯上不应该引用private函数或变量
+__author__ = 'Daicy'
+
+import sys  # 导入模块
+
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, world!')
+    elif len(args) == 2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
+
+# 当我们在命令行运行hello模块文件时，Python解释器把一个特殊变量__name__置为__main__
+# 而如果在其他地方导入该hello模块时，if判断将失败，因此，这种if测试可以让一个模块通过命令行运行时执行一些额外的代码，最常见的就是运行测试。
+if __name__ == '__main__':
+    test()
