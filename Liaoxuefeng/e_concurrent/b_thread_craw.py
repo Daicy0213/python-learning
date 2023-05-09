772b24a3f6481d2756733ef66839249ee28df6ef
Dacong <372454567@qq.com>
2023-05-09 16:11:57 +0800

    thread create

---
+++ Liaoxuefeng/e_concurrent/b_thread_craw.py
@@ -0,0 +1,60 @@
+"""
+使用单线程和多线程爬取网页
+"""
+
+import requests
+import threading
+import time
+
+# 访问以下50个网页
+urls = [
+    f'https://www.cnblogs.com/#p{page}'
+    for page in range(1, 50 + 1)
+]
+
+# 结果: ['https://www.cnblogs.com/#p1', 'https://www.cnblogs.com/#p2',....]
+print(urls)
+
+
+def craw(url):
+    r = requests.get(url)
+    print(url, len(r.text))
+
+
+# 使用单线程爬取50个网页
+def single_thread():
+    start_time = time.time()
+    for url in urls:
+        craw(url)
+    print(f'单线程爬取的时间为{time.time() - start_time}s')
+
+
+# 使用多线程爬取
+def multi_thread():
+    start_time = time.time()
+    threads = []
+    for url in urls:
+        threads.append(
+            threading.Thread(target=craw, args=(url,))
+        )
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print(f'多线程爬取的时间为{time.time() - start_time}s')
+
+
+single_thread()
+# 结果:
+# https://www.cnblogs.com/#p1 69682 .....
+# https://www.cnblogs.com/#p50 69682
+# 单线程爬取的时间为11.415833711624146s
+
+multi_thread()
+# 结果:
+# https://www.cnblogs.com/#p1 69682
+# https://www.cnblogs.com/#p50 69682
+# 多线程爬取的时间为0.3929774761199951s
